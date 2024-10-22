d793df86321311e413fbbf5255f2428ec2a4e21a
mcaldel <mcalzadodelgado@gmail.com>
2024-10-22 19:14:53 +0200

    Create mcaldel.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/mcaldel.js
@@ -0,0 +1,23 @@
+// https://www.javascript.com
+
+// Este es un comentario de una sola linea.
+
+/* 
+Este es
+un comentario
+en varias
+lineas
+*/
+
+let numero;
+var numero1;
+const numero2 = 1234;
+
+let string = "Hola Mundo";
+let booleans = true;
+let integer = 1;
+let float = 1.2;
+let isNAN = "Valor obligatorio";
+let prompt = "¿Esto es una Pregunta?";
+
+console.log("Hola JavaScript");
